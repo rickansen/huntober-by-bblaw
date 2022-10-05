22c03d4ef4a91a74653b2b0f7276aa1d1684d436
Andersen <rickansenn@gmail.com>
2022-10-05 11:20:14 +0800

    Refactored day 3 function name

--- week-1--string/day-3.js
+++ week-1--string/day-3.js
@@ -1,9 +1,9 @@
-function decodeStringTwo(str) {
+function decodeStringThree(str) {
   return str.replaceAll(/[Space]/g, ' ');
 }
 
 console.log(
-  decodeStringTwo(
+  decodeStringThree(
     'e!!Igv)t5lltBcvbdeDH3dVw!OOtI#Aa.ZMDu7WYpP^VVjDc4I50iv#ylhgmQfs'
   )
 );

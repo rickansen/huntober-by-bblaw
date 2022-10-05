181b6f763a2f7a124fe2e9c723d428da6a7cd12b
Andersen <rickansenn@gmail.com>
2022-10-05 11:20:00 +0800

    Compiled all solutions

---
+++ complied-solutions.js
@@ -0,0 +1,57 @@
+function decodeString(str) {
+  let convert = {
+    0: 'O',
+    1: 'I',
+    2: 'Z',
+    3: 'E',
+    4: 'h',
+    5: 'S',
+    6: 'G',
+    7: 'L',
+    8: 'B',
+    9: 'q',
+  };
+
+  return [...str]
+    .map((x) =>
+      x in convert
+        ? convert[x]
+        : Object.values(convert).includes(x)
+        ? Object.entries(convert).find((a) => a[1] === x)[0]
+        : x
+    )
+    .join('');
+}
+
+function decodeStringTwo(str) {
+  return [...str.slice(str.indexOf('c') + 1).replaceAll('Eek!', '')]
+    .reverse()
+    .join('')
+    .split('v')
+    .slice(1)
+    .map((x) => x[0])
+    .join('');
+}
+
+function decodeStringThree(str) {
+  return str.replaceAll(/[Space]/g, ' ');
+}
+
+function decodeStringFour(str) {
+  return [...str].reverse().join('');
+}
+
+function compiledDecodeFuncs(str) {
+  const day1 = decodeString(str);
+  const day3 = decodeStringThree(day1);
+  const day4 = decodeStringFour(day3);
+  return day4;
+}
+
+console.log(
+  compiledDecodeFuncs(
+    'e!!Igv)t5lltBcvbdeDH3dVw!OOtI#Aa.ZMDu7WYpP^VVjDc4I50iv#ylhgmQfs'
+  )
+);
+
+// string to decrypt - "e!!Igv)t5lltBcvbdeDH3dVw!OOtI#Aa.ZMDu7WYpP^VVjDc4I50iv#ylhgmQfs"
